6b874c8aca11776c6ad2c23d195a49ba40d7f80f
bast0588 <100563464+bast0588@users.noreply.github.com>
2026-06-08 14:12:17 +0200

    Functioner er tilføjet til headerfiler

--- karakter.h
+++ karakter.h
@@ -1,10 +1,18 @@
 #ifndef KARAKTER_H
 #define KARAKTER_H
-
+#include <string>
+#include "monster.h"
 class karakter{
 
 public:
     karakter();
+    Monster getMonster(int i);
+    std::string getNavn();
+    void setNavn(std::string nytNavn);
+
+private:
+    std::string navn;
+    Monster monstre[4];
 };
 
 #endif // KARAKTER_H
--- monster.h
+++ monster.h
@@ -1,9 +1,18 @@
 #ifndef MONSTER_H
 #define MONSTER_H
+#include <string>
 
 class Monster {
 public:
     Monster();
+    void setHp(int nyHp);
+    int getHp();
+    int getStyrke();
+    void setStyrke(int nyStyrke);
+private:
+    std::string navn;
+    int hp;
+    int styrke;
 };
 
 #endif // MONSTER_H
